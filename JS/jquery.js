3cb06691261873e7495c3d3c9f352ac4ac6d2b45
hdnjs <khw734100@gmail.com>
2022-12-06 12:46:03 +0900

    1206

--- JS/jquery.js
+++ JS/jquery.js
@@ -40,4 +40,13 @@ $(function () {
   };
 
   $.getJSON("/data/gallery.json", getGalleryData);
+
+  // Navigation Moving to Target Section
+  $(".nav-lists li").on("click", function () {
+    const targetIdx = $(this).index();
+    // console.log(targetIdx);
+    const pagePosition = $(".nav-target").eq(targetIdx).offset().top;
+
+    $("html, body").animate({ scrollTop: pagePosition }, 300);
+  });
 }); //  recommend
