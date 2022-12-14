99efcade161b41a4916d8b10d2fdf26ebd35116f
hdnjs <khw734100@gmail.com>
2022-12-14 12:45:07 +0900

    1214_backend

---
+++ sql/spl_products.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- 생성 시간: 22-12-13 02:21
+-- 서버 버전: 10.4.27-MariaDB
+-- PHP 버전: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- 데이터베이스: `soaply`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 테이블 구조 `spl_products`
+--
+
+CREATE TABLE `spl_products` (
+  `pro_idx` int(11) NOT NULL COMMENT '상품 고유번호',
+  `pro_name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '상품 이름',
+  `pro_desc` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '상품 설명',
+  `pro_img` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '상품 사진',
+  `pro_reg` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '등록일'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 덤프된 테이블의 인덱스
+--
+
+--
+-- 테이블의 인덱스 `spl_products`
+--
+ALTER TABLE `spl_products`
+  ADD PRIMARY KEY (`pro_idx`);
+
+--
+-- 덤프된 테이블의 AUTO_INCREMENT
+--
+
+--
+-- 테이블의 AUTO_INCREMENT `spl_products`
+--
+ALTER TABLE `spl_products`
+  MODIFY `pro_idx` int(11) NOT NULL AUTO_INCREMENT COMMENT '상품 고유번호';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
