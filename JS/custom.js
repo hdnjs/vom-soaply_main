2129c59b90d76bd56ce207ae7df2d858fab7fc0d
hdnjs <khw734100@gmail.com>
2022-12-23 12:04:02 +0900

    1223 rebackup

--- JS/custom.js
+++ JS/custom.js
@@ -13,7 +13,7 @@ window.addEventListener("load", function () {
 const productsBox = document.querySelector(".products");
 
 const getData = async () => {
-  await fetch("/main_backend/model/get_products.php")
+  await fetch("/main_backend/model/requestProduct.php")
     .then((response) => response.json())
     .then((data) => {
       let dataEl;
